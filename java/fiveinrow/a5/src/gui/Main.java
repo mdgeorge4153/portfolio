a07662e50bc6b80248eb744b7da3b5a791b6caad
Michael George <mdgeorge@cs.cornell.edu>
2020-11-05 11:40:59 -0500

    five in a row project copied from 2110 archive, 16fa

---
+++ java/fiveinrow/a5/src/gui/Main.java
@@ -0,0 +1,12 @@
+/* NetId(s): djg17, ret87. Time spent: hh hours, mm minutes. */
+
+package gui;
+
+import model.NotImplementedException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		throw new NotImplementedException();
+	}
+}
